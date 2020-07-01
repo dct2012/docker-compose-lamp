2784394afed0864f8f90db48427646441d9232a9
Cameron Taaffe <cameron.taaffe@gmail.com>
2020-07-01 18:24:37 -0400

    Database now builds itself.

--- _php/html/app/Config.php
+++ _php/html/app/Config.php
@@ -5,18 +5,19 @@ namespace App;
 
 
 class Config {
+	protected string $dbName;
 	protected string $dbUserName;
 	protected string $dbPassword;
 	protected string $dbDsn;
 
 	public function __construct( array $config ) {
+		$this->dbName     = $config[ 'dbName' ];
 		$this->dbUserName = $config[ 'dbUserName' ];
 		$this->dbPassword = $config[ 'dbPassword' ];
 
 		$dbHost      = $config[ 'dbHost' ];
 		$dbPort      = $config[ 'dbPort' ];
-		$dbName      = $config[ 'dbName' ];
-		$this->dbDsn = "mysql:host=$dbHost;port=$dbPort;dbname=$dbName";
+		$this->dbDsn = "mysql:host=$dbHost;port=$dbPort;dbname=$this->dbName";
 	}
 
 	/** @return string */
@@ -24,13 +25,18 @@ class Config {
 		return $this->dbDsn;
 	}
 
-	/** @return mixed|string */
-	public function getDbPassword() {
+	/** @return string */
+	public function getDbPassword(): string {
 		return $this->dbPassword;
 	}
 
-	/** @return mixed|string */
-	public function getDbUserName() {
+	/** @return string */
+	public function getDbUserName(): string {
 		return $this->dbUserName;
+	}
+
+	/** @return string */
+	public function getDbName(): string {
+		return $this->dbName;
 	}
 }
--- _php/html/app/DB.php
+++ _php/html/app/DB.php
@@ -4,21 +4,74 @@
 namespace App;
 
 use PDO;
-use Exception;
 
 class DB {
 	protected PDO $PDO;
+	protected Config $Config;
 
 	public function __construct( Config $Config ) {
-		try {
-			$this->PDO = new PDO( $Config->getDbDsn(), $Config->getDbUserName(), $Config->getDbPassword() );
-		} catch( Exception $e ) {
-			var_dump( $e->getMessage() );
+		$this->Config = $Config;
+		$this->PDO    = new PDO(
+			$this->Config->getDbDsn(),
+			$this->Config->getDbUserName(),
+			$this->Config->getDbPassword()
+		);
+
+		if( !$this->doesGreetingsTableExist() ) {
+			if( $this->createGreetingsTable() ) {
+				$this->populateGreetingTable();
+			}
 		}
 	}
 
+	/** @return string */
 	public function getGreeting(): string {
-		$result = $this->PDO->query( 'SELECT greeting FROM greetings WHERE id = 1', PDO::FETCH_ASSOC )->fetch();
+		$stmt   = 'SELECT greeting FROM greetings WHERE id = 1';
+		$result = $this->PDO->query( $stmt, PDO::FETCH_ASSOC )->fetch();
+
 		return $result[ 'greeting' ];
+	}
+
+	/** @return bool */
+	protected function doesGreetingsTableExist(): bool {
+		return $this->doesTableExist( 'greetings' );
+	}
+
+	/**
+	 * @param string $tableName
+	 * @return bool
+	 */
+	protected function doesTableExist( string $tableName ): bool {
+		$dbName = $this->Config->getDbName();
+		$stmt   = "SHOW TABLES WHERE `Tables_in_$dbName` = '$tableName'";
+		$result = $this->PDO->query( $stmt, PDO::FETCH_ASSOC )->fetch();
+
+		return !empty( $result );
+	}
+
+	/** @return bool */
+	protected function createGreetingsTable(): bool {
+		$dbName = $this->Config->getDbName();
+		$stmt   = "CREATE TABLE $dbName.greetings (
+	id INT UNSIGNED auto_increment NOT NULL,
+	greeting varchar(255) NOT NULL,
+	CONSTRAINT greetings_PK PRIMARY KEY (id),
+	CONSTRAINT greetings_UN UNIQUE KEY (greeting)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+CREATE UNIQUE INDEX greetings_id_IDX USING BTREE ON $dbName.greetings (id);";
+		$this->PDO->query( $stmt, PDO::FETCH_ASSOC )->fetch();
+
+		return $this->doesGreetingsTableExist();
+	}
+
+	/** @return bool */
+	protected function populateGreetingTable(): bool {
+		$stmt   = "INSERT INTO `greetings` VALUES (1,'Hello World, from MariaDB!')";
+		$result = $this->PDO->query( $stmt, PDO::FETCH_ASSOC )->fetch();
+
+		return !empty( $result );
 	}
 }
